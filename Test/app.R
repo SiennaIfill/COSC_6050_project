ffcc650219ac5d565ae84a0e56ef95bde67e5cda
SiennaIfill <sienna.ifill@marquette.edu>
2025-10-03 22:53:14 -0500

    Working dropdown menus

--- Test/app.R
+++ Test/app.R
@@ -6,16 +6,21 @@
 #
 #    https://shiny.posit.co/
 #
-
+library(tidyverse)
 library(shiny)
-#available_teams <- c("marquette","ball_state","florida","jmu","hawaii","san_diego","minnesota","utah_state","uwm","wisconsin","wku")
+library(readr)
+library(dplyr)
 data <- read_csv("~/COSC_6050_project/marquette_plays.csv")
 teams <- data |> distinct(team)
 teams <- na.omit(teams)
-available_teams <- teams |> pull(team)
+available_teams <- teams |>  pull(team)
+available_teams <- sort(available_teams)
 players <- data |> filter(team == "Marquette University") |> distinct(Name)
 players <- na.omit(players)
 available_players <- players |> pull(Name)
+available_players <- sort(available_players)
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -23,11 +28,12 @@ ui <- fluidPage(
     titlePanel("Player Patterns"),
 
     # Dropdown attempt
-    selectInput("Team","Choose Team to Scout", choices = available_teams),
+    selectInput("scout_team","Choose Team to Scout", choices = c("",available_teams), multiple = F),
     
     # this will be redone later
-    selectInput("Player", "Choose Player to Scout", choices = available_players),
-    
+    #selectInput("Player", "Choose Player to Scout", choices = NULL),
+    #selectizeInput("scout_player","Choose Player to Scout", choices=c("",available_players)),
+    selectizeInput("scout_player","Choose Player to Scout", choices=NULL),
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
@@ -46,8 +52,20 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
+server <- function(input, output, session) {
+    
+  observeEvent(input$scout_team, {
+    if (input$scout_team == ""){
+      available_players <- c("")
+    }else{
+      players <- data |> filter(team == input$scout_team) |> distinct(Name)
+      players <- na.omit(players)
+      available_players <- players |> pull(Name)
+      available_players <- sort(available_players) 
+    }
+    updateSelectizeInput(session, "scout_player",choices=available_players)
+  })
+    
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
